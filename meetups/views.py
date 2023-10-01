b8c3d3b2c8b6475a520f8ab68c946c71d63fed5e
Raj Aryan Singh <rajlysm15@gmail.com>
2023-10-02 01:44:44 +0530

    meetups list

--- meetups/views.py
+++ meetups/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse ('hello from django!')
+    meetups = [
+        {"title": "web3 meetup", "location": "Bengaluru", "slugs": "web3-meetup"},
+        {"title": "ai meetup", "location": "delhi", "slugs": "ai-meetups"},
+    ]
+    return render(request, "meetups/index.html", {
+        'meetups': meetups
+    })
